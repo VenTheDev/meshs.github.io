97b4b4ffb15222c17053cb8a18ebb89cd5e09e51
VenTheDev <tamayo.jrayven@gmail.com>
2026-07-02 11:35:09 -0700

    s

--- script.js
+++ script.js
@@ -9,3 +9,31 @@ searchBtn.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
     searchBox.style.display = "none";
 });
+
+const images = [
+    "assets/images/background1.png",
+    "assets/images/background2.png",
+    "assets/images/background3.png"
+];
+
+const bg1 = document.getElementById("bg1");
+const bg2 = document.getElementById("bg2");
+
+let current = 0;
+let showingFirst = true;
+
+setInterval(() => {
+    current = (current + 1) % images.length;
+
+    if (showingFirst) {
+        bg2.src = images[current];
+        bg2.classList.add("active");
+        bg1.classList.remove("active");
+    } else {
+        bg1.src = images[current];
+        bg1.classList.add("active");
+        bg2.classList.remove("active");
+    }
+
+    showingFirst = !showingFirst;
+}, 5000); // Change every 5 seconds
