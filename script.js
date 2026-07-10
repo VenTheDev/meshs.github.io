c4c969721300de5c59a67e6e9d0bb6e410f062da
VenTheDev <tamayo.jrayven@gmail.com>
2026-07-10 11:32:31 -0700

    s

--- script.js
+++ script.js
@@ -1,39 +1,28 @@
-const searchBtn = document.getElementById("searchBtn");
-const searchBox = document.getElementById("searchBox");
-const closeBtn = document.getElementById("closeBtn");
+const slides = document.querySelectorAll(".slide");
+const dots = document.querySelectorAll(".slider-nav a");
 
-searchBtn.addEventListener("click", () => {
-    searchBox.style.display = "flex";
+let currentSlide = 0;
+
+function showSlide(index) {
+    slides[currentSlide].classList.remove("active");
+    dots[currentSlide].classList.remove("active");
+
+    slides[index].classList.add("active");
+    dots[index].classList.add("active");
+
+    currentSlide = index;
+}
+
+slides[0].classList.add("active");
+dots[0].classList.add("active");
+
+dots.forEach((dot, index) => {
+    dot.addEventListener("click", (e) => {
+        e.preventDefault();
+        showSlide(index);
+    });
 });
-
-closeBtn.addEventListener("click", () => {
-    searchBox.style.display = "none";
-});
-
-const images = [
-    "assets/images/background1.png",
-    "assets/images/background2.png",
-    "assets/images/background3.png"
-];
-
-const bg1 = document.getElementById("bg1");
-const bg2 = document.getElementById("bg2");
-
-let current = 0;
-let showingFirst = true;
 
 setInterval(() => {
-    current = (current + 1) % images.length;
-
-    if (showingFirst) {
-        bg2.src = images[current];
-        bg2.classList.add("active");
-        bg1.classList.remove("active");
-    } else {
-        bg1.src = images[current];
-        bg1.classList.add("active");
-        bg2.classList.remove("active");
-    }
-
-    showingFirst = !showingFirst;
-}, 5000); // Change every 5 seconds
+    showSlide((currentSlide + 1) % slides.length);
+}, 10000);
---
+++ student/onlineportal/script.js
@@ -0,0 +1,4 @@
+let x = 12;
+let y = 3;
+
+console.log(x/y+x);
